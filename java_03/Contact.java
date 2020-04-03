735baabab46034a751ac4ce84c9b68499a12e772
phuong2001 <tranvanphuong01012001@gmail.com>
2020-04-03 21:28:31 +0700

    contaclist

---
+++ java_03/Contact.java
@@ -0,0 +1,16 @@
+package java_03;
+
+public class Contact {
+    protected String name;
+    protected String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "name= " + name + ", phone= " + phone ;
+    }
+}
